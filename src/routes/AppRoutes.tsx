5056e8b981fbe3be6cb49b3bbeae6219da6be16a
joselenon <jjoseguilhermeneto@gmail.com>
2024-02-10 19:13:01 -0300

    Starting Timers

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Timers from '../pages/Timers';
 import UserProfile from '../pages/UserProfile';
 
 export type TParams = 'username';
@@ -10,6 +11,7 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/timers" element={<Timers />} />
       <Route path="/profile/:username" element={<UserProfile />} />
     </Routes>
   );
